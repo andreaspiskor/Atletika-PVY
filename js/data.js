6f480ff1ab7fddb670f0275d9354a4ad7d0574b6
Andreas Piskoř <36028797+andreaspiskor@users.noreply.github.com>
2019-03-22 01:16:21 +0100

    Version 1.01
    LEL

--- js/data.js
+++ js/data.js
@@ -45,3 +45,37 @@ var atleti = [
         "link":"https://cs.wikipedia.org/wiki/Denisa_Rosolov%C3%A1" 
     },                  
 ];
+
+var zajimavosti = [
+    {
+        "title":"Mistři ČR v Atletice SOKOP 2018",
+        "photo":"2018.jpg"
+    },
+    {
+        "title":"Mistři ČR v Atletice SOKOP 2017",
+        "photo":"2017.jpg"
+    },
+    {
+        "title":"Mistři ČR v Atletice SOKOP 2016",
+        "photo":"2016.jpg"
+    }
+];
+
+var perlicky = [
+    {
+        "title":"Barbora Malíková",
+        "text":"je česká atletka-běžkyně. Závodí za TJ Sokol Opava, její trenérkou je Jana Gellnerová. V roce 2017 vyhrála ve svých 15 letech Mistrovství světa v atletice do 17 let na trati 400 metrů a stala se tak nejmladší vítězkou na této trati v historii. Ve stejném roce a na stejné trati vyhrála i Mistrovství ČR žen, o rok později vyhrála Mistrovství Evropy v atletice do 17 let. Po Zuzaně Hejnové, Kateřině Cachové a Michaele Hrubé se stala teprve čtvrtou českou dorosteneckou mistryní světa. S časem 52,66 je držitelkou českého dorosteneckého rekordu na trati 400 m, dosavadní rekord Naděždy Tomšové překonala po 29 letech, když v roce 2017 v čase 52,74 vyhrála Mistrovství světa do 17 let. O rok později rekord o osm setin vylepšila výhrou na Mistrovství Evropy do 17 let. S časem 2:03,51 je také držitelkou českého dorosteneckého rekordu na trati 800 m, zaběhla jej v roce 2018 na českém dorosteneckém mistrovství. V roce 2017 byla v české anketě Atlet roku vyhlášena objevem roku.",
+    },
+    {
+        "title":"Tereza Šínová",
+        "text":"Talentovaná závodnice oddílu Atletika Jižní Město z.s. TEREZA ELENA ŠÍNOVÁ 01 se stala mistryní ČR v halovém pětiboji v osobním rekordu 4.047 bodů.Tato studentka II.ročníku Gymnázia Opatov pět prokázala své vysoké předpoklady a potvrdila, že patří mezi výrazné talenty české atletiky, když zvítězila s velkým náskokem 300 bodů. Soutěží prošla s následujícími výkony: 60m přek. 8,63s =OR- výška 171cm- koule 4kg 11,28m- dálka 602cm OR v hale- 800m- 2:27,80s Zařadila se na šestice elitních čtyřtisícových vícebojařek juniorek v dlouhodobých tabulkách, těsně za Elišku Klučinovou a v příštím roce, kdy bude ještě závodit v kategorii juniorek jistě zaútočí na překonání českého rekordu. Trenérem Terezy je od jejich atletických začátků Ladislav Kárský.",
+    },
+    {
+        "title":"Tadeáš Formánek",
+        "text":"V současnosti je snad k neporažení. Na Olympiádě dětí a mládeže v běhu na 60 metrů vyhrál svůj rozběh, semifinále i finále, bez jediného zaváhání. „Tohle jsou moje nejlepší závody,“ radoval se Tadeáš Formánek, reprezentant Moravskoslezského kraje, v cíli. V Brně tak úspěšně navázal na předchozí úspěchy z mistrovství republiky. Letos už se totiž stal dvojnásobným žákovským šampionem, ale do Brna nemířil jen za zlatem. Hlavně chtěl konečně pokořit hranici sedmi vteřin. A to se mu povedlo už v pondělí. Nejprve mladý sprinter doběhl v čase 6,98, v následném semifinále se ještě zlepšil na parádních 6,93. Český žákovský rekord je lepší jen o pouhých osm setin. Ve finále se Tadeáš sice zase vrátil nad zmíněnou hranici, ale kdo by se zlobil – zase byl nejrychlejší a získal zlato. „Je tady nádherný stadion, perfektní tartan a teď se k tomu přidal i ideální vítr do zad. Jsem naprosto spokojený,“ sypal ze sebe bezprostředně po finále, ještě zadýchaný.",
+    },
+    {
+        "title":"Tomáš Kratochvíl",
+        "text":"Je mu teprve patnáct let a už nyní patří mezi největší přísliby české atletiky. Skokan do dálky Tomáš Kratochvíl naposledy zaujal na Olympiádě dětí a mládeže v Brně, kde o 12 centimetrů vylepšil 75 let starý rekord. Ve finále soutěže skočil 724 cm. Výkon Jaroslava Fikejze byl dlouho tím nejlepším, co v žákovské kategorii česká atletika viděla. Jeho 712 centimetrů mělo o to větší hodnotu, že v roce 1942 skákal Fikejz na škvárovém rozběžišti. S dnešními podmínkami se ty jeho srovnat nedají. Rekordní tabulky přesto nerozlišují, na jakém povrchu kdo skákal. A tak byl Fikejzův výkon celých 75 let českým žákovským rekordem. Teď ho přepsal patnáctiletý Tomáš Kratochvíl. Na Olympiádě dětí a mládeže v Brně skočil 724 centimetrů. Kratochvíl je mimořádný talent české atletiky. Nejenže nyní vylepšil svůj osobní rekord v dálce (ten starý činil 694 centimetrů), vedle dálky se věnuje také sprintům a výšce, kde už přeskočil hranici 196 centimetrů.",
+    }
+];
